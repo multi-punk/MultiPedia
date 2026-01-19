6375cbb277b0f4bfec1b17c0eb39be75feb064d6
JORICK404 <jokerguys221@gmail.com>
2026-01-19 13:11:44 +0300

    Refactor and expand guides structure, add government section
    Moved 'cooking' and 'dungens_and_creepers' content under 'guides', reorganizing the directory structure for better clarity. Added a new 'government' section with detailed documentation on the judicial system.

--- content/_meta.js
+++ content/_meta.js
@@ -1,27 +1,22 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-      about: {
-        title: 'Правила',
-        type: 'page',
-        href: '/rules'
-      },
-      contact: {
-        title: 'Гайды',
-        type: 'page',
-        href: '/guides'
-      },
-  rules: "Правила",
-  
+  index: {
+    title: "Главная"
+  },
+  rules: {
+    title: "Правила"
+  },
+  "---": {
+    type: "separator",
+    title: "MP SMP"
+  },
   begginer_course: {
     title: 'Курс для новичков'
   },
   guides: {
-    title: 'Гайды'
+    title: 'Гайды',
+    theme: {
+      collapsed: false
+    }
   },
-  cooking: 'Да будет пир',
-  dungens_and_creepers: 'Подземелье и Криперы',
-  "###": {
-    type: "separator",
-  },
-  
 };
---
+++ content/guides/government/_meta.js
@@ -0,0 +1,3 @@
+export default {
+  'judgment-system': '⚖️ Судебная система',
+}
